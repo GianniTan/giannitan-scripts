52b240a4c2d1f66e9b2bc2af3e84df93b08cb936
GianniTan <contact@giannitan.com>
2025-06-17 22:09:28 +0200

    Update split-type-new.js

--- split-type-new.js
+++ split-type-new.js
@@ -1,1 +1,16 @@
-test
+gsap.registerPlugin(ScrollTrigger);
+
+const splitTypes = document.querySelectorAll('.scroll-highlight');
+splitTypes.forEach((char,i) => {
+  const text = new SplitType(char, {types: ['chars','words']});
+  gsap.from(text.chars, {
+    scrollTrigger: {
+      trigger: char,
+      start: 'top 80%',
+      end: 'top 50%',
+      scrub: true,
+    },
+    opacity: 0.2,
+    stagger: 0.1,
+  })
+});
