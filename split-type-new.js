e8153ac78ea01849efa7bf6567e63cbe74849257
GianniTan <contact@giannitan.com>
2025-06-17 15:24:11 +0200

    Update split-type-new.js

--- split-type-new.js
+++ split-type-new.js
@@ -1,36 +1,39 @@
+gsap.registerPlugin(ScrollTrigger);
+
 let text;
 
-function runSplit() {
-  let currentElement = $(".split-lines");
+function initSplitAndAnimation() {
+  if (text) text.revert();
+
+  const currentElement = $(".split-lines");
   text = new SplitType(currentElement, { types: 'lines, words' });
-  $( ".line" ).append( "<div class='line-mask'></div>" );
+  $(".line").append("<div class='line-mask'></div>");
+
   runAnimation();
+
+  // refresh ScrollTrigger om alles direct goed te zetten
+  ScrollTrigger.refresh();
 }
 
-runSplit();
-
-window.addEventListener('resize', function() {
-  text.revert();
-  runSplit();
-});
-
 function runAnimation() {
-  $(".line").each(function (index) {
-    let triggerElement = $(this);
-    let targetElement = $(this).find('.line-mask ');
-  
-    let tl = gsap.timeline({
+  $(".line").each(function () {
+    const triggerElement = $(this);
+    const targetElement = triggerElement.find('.line-mask');
+
+    gsap.timeline({
       scrollTrigger: {
         trigger: triggerElement,
-        // trigger element - viewport
         start: "top center",
         end: "bottom center",
         scrub: 1
       }
-    });
-    tl.to(targetElement, {
-      width: "0%",
-      duration: 1
-    });
+    })
+    .to(targetElement, { width: "0%", duration: 1 });
   });
 }
+
+// init na volledige page-load
+window.addEventListener('load', initSplitAndAnimation);
+
+// opnieuw init bij venster-resize
+window.addEventListener('resize', initSplitAndAnimation);
